63188ba307624c3e7875e9ea75807d7a57a448b6
Cyalline-Yang <62007767+Cyalline-Yang@users.noreply.github.com>
2020-05-26 19:07:51 +0800

    Add files via upload

---
+++ 脚本/environ.sh
@@ -0,0 +1,17 @@
+
+
+# WHITE_IP
+# SSH_PORT
+user="sammy"
+USER_DIR="/home/$user/ftp"
+ANONY_DIR="/var/ftp/pub"
+TMP_DIR="/home/hkl/auto"
+nfs_general_srv="/var/nfs/general"
+nfs_home_srv="/home"
+nfs_srv="192.168.178.101"
+nfs_clt="192.168.178.100"
+nfs_general_clt="/nfs/general"
+nfs_home_clt="/nfs/home"
+
+smbuser="demoUser"
+smbgroup="demoGroup"
